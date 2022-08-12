7a157dea5d2ccc833da08eb6c3198e58e16d7a7b
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-12 16:43:34 -0400

    added correct code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(x < 0 || y < 0)
+        return "ERROR";
+    if(typeof x !== 'number' || typeof y !== 'number')
+        return "ERROR";
+    let result = 0;
+    if(y > x) {
+        for(let i = x; i <= y; i++) {
+            result += i; 
+        }
+    }
+    if(x > y) {
+        for(let i = y; i <= x; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
