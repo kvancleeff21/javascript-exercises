4fe5d38c1a3bb5a687e0fae36c39d8ff5478d96f
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-12 12:10:05 -0400

    added correct code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if(string == " ") {
+        return " ";
+    }
+    let reversedString = "";
+    let length = string.length;
+    for(let i = length - 1; i >= 0; i--) {
+        reversedString += string.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
