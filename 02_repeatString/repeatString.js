e4ecde377f572ad8abf8c1bc046e3ce4ac196fcb
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-12 12:00:47 -0400

    finished implementation

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(message, number) {
+    if(number < 0) {
+        return "ERROR";
+    }
+    let answer = "";
+    for(let i = 0; i < number; i++) {
+        answer += message;
+    }
+    return answer;
 };
 
 // Do not edit below this line
