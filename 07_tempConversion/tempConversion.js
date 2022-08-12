5e43af1ce0a4f5369d008ed994cc0675dd059608
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-12 17:01:48 -0400

    added correct code to both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = temp - 32;
+  let divisor = 5 / 9;
+  let result = newTemp * divisor;
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let divisor = 9/5;
+  let newTemp = temp * divisor;
+  let result = newTemp + 32;
+  let resultRounded = Math.round(result * 10) / 10;
+  return resultRounded;
 };
 
 // Do not edit below this line
